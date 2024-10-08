a277ab636b76835761adee4a7376b2a2fa8bd314
Francesco <francesco@cactus.polito.it>
2024-10-08 16:55:59 +0200

    Add Polipeople commands

--- src/Commands/PolipeopleCommand.php
+++ src/Commands/PolipeopleCommand.php
@@ -6,14 +6,81 @@ use Illuminate\Console\Command;
 
 class PolipeopleCommand extends Command
 {
-    public $signature = 'polipeople';
+    protected $signature = 'polipeople
+                            {action : The action to perform (install, publish-views, publish-config)}
+                            {--force : Force the operation to run when publishing views}';
 
-    public $description = 'My command';
+    protected $description = 'Manage Polipeople plugin';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $action = $this->argument('action');
 
+        switch ($action) {
+            case 'install':
+                return $this->installPolipeople();
+            case 'publish-views':
+                return $this->publishViews();
+            case 'publish-config':
+                return $this->publishConfig();
+            default:
+                $this->error("Invalid action. Use 'install', 'publish-views', or 'publish-config'.");
+                return self::FAILURE;
+        }
+    }
+
+    protected function installPolipeople(): int
+    {
+        $this->info('Installing Polipeople...');
+
+        // Publish migration files
+        $this->call('vendor:publish', [
+            '--tag' => 'polipeople-migrations'
+        ]);
+
+        // Run migrations
+        $this->call('migrate', ['--path' => 'vendor/detit/polipeople/database/migrations']);
+
+        // Publish config
+        $this->call('vendor:publish', [
+            '--provider' => 'Detit\Polipeople\PolipeopleServiceProvider',
+            '--tag' => 'polipeople-config'
+        ]);
+
+        $this->info('Polipeople has been installed successfully!');
+        $this->info('You can now publish the views if you want to customize them.');
+        return self::SUCCESS;
+    }
+
+    protected function publishViews(): int
+    {
+        $this->info('Publishing Polipeople views...');
+
+        $params = [
+            '--provider' => 'Detit\Polipeople\PolipeopleServiceProvider',
+            '--tag' => 'polipeople-views'
+        ];
+
+        if ($this->option('force')) {
+            $params['--force'] = true;
+        }
+
+        $this->call('vendor:publish', $params);
+
+        $this->info('Polipeople views have been published successfully!');
+        return self::SUCCESS;
+    }
+
+    protected function publishConfig(): int
+    {
+        $this->info('Publishing Polipeople config...');
+
+        $this->call('vendor:publish', [
+            '--provider' => 'Detit\Polipeople\PolipeopleServiceProvider',
+            '--tag' => 'polipeople-config'
+        ]);
+
+        $this->info('Polipeople config has been published successfully!');
         return self::SUCCESS;
     }
 }
